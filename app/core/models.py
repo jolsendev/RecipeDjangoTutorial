afddf0259bf6a1e33992370ea625b60ee8d35a7e
Jamie Olsen <jwolsen@mmm.com>
2019-03-13 13:01:51 -0600

    added normalize helper test for email

--- app/core/models.py
+++ app/core/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
+# https://docs.djangoproject.com/en/2.1/topics/auth/customizing/#django.contrib.auth.models.BaseUserManager.normalize_email
 
 
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create and saves a new User"""
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
 
